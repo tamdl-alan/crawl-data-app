f671d472be7c9a5f6eec9be20153d60f4dcc5b09
Tam Dang Linh <linhtam.tamdl@gmail.com>
2025-08-23 13:41:22 +0700

    Add front end proj

---
+++ front/src/menuAside.js
@@ -0,0 +1,32 @@
+import {
+  // mdiAccountCircle,
+  mdiMonitor,
+  // mdiGithub,
+  // mdiLock,
+  // mdiAlertCircle,
+  mdiSquareEditOutline,
+  mdiTable,
+  // mdiViewList,
+  // mdiTelevisionGuide,
+  // mdiResponsive,
+  // mdiPalette,
+  // mdiReact,
+} from '@mdi/js'
+
+export default [
+  {
+    to: '/dashboard',
+    icon: mdiMonitor,
+    label: 'Dashboard',
+  },
+  {
+    to: '/tables',
+    label: 'Tables',
+    icon: mdiTable,
+  },
+  {
+    to: '/forms',
+    label: 'Forms',
+    icon: mdiSquareEditOutline,
+  },
+]
